d1bf4018ea412f3c7d8393f494812a7fc2e76b43
Attila Horvath <sweeptic@gmail.com>
2025-03-05 13:22:17 +0100

    012 Understanding Layout Metadata

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import Header from '@/components/header';
 import './globals.css';
 
+// default metadata
+// in case of override: title replaced, description is merged
 export const metadata = {
   title: 'NextPosts',
   description: 'Browse and share amazing posts.',
